ac88b30089f61b78c701836d28c1e71d40ca2425
alexander alejo <aa3590@live.com>
2024-11-13 19:42:15 -0400

    comentario 2

--- ejercico1.1git.py
+++ ejercico1.1git.py
@@ -7,3 +7,9 @@ print("hola mundo git")
 print("hola mundo desde casa")
 
 
+cantidad = 0 
+cantidad +=1
+while True:
+    print("Estoy cambiando la cantidad a {cantida}")
+    if cantidad == 10:
+        break
